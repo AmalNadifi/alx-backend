6ec16bc3849f0f409ab33d3b27f04d8973249daf
AmalNadifi <amal.nadifi9@gmail.com>
2024-03-12 15:29:33 +0000

    added shebang and comments to the file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+""" Thefollowing module is a basic flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,8 +7,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Render the index.html template.
+
+    Returns:
+        str: Rendered HTML content.
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
