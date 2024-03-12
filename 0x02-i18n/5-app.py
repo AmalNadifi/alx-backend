969645e99d8e409f0a6d53fd3e4e89d8a9e5c4ed
AmalNadifi <amal.nadifi9@gmail.com>
2024-03-12 17:33:32 +0000

    Updated get_locale function

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -52,6 +52,11 @@ def get_locale():
     """
     Select the best language for the user based on their request.
     """
+    # Check if the 'locale' parameter is in the URL and its a supported locale
+    requested_locale = request.args.get('locale')
+    if requested_locale in app.config['LANGUAGES']:
+        return requested_locale
+    # If not or if the parameter is not present, use default behavior
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
