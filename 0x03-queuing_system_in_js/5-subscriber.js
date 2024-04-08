2953e8c1dcab16f513ef025232e83a80ffa9ce4e
AmalNadifi <amal.nadifi9@gmail.com>
2024-04-08 20:30:31 +0000

    Task5. Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env yarn dev
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+  client.subscribe('holberton school channel');
+});
+
+client.on('error', (err) => {
+  console.error('Redis client not connected to the server:', err.message);
+});
+
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
