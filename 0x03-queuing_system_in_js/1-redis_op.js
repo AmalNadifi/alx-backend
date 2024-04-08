40c1933f824c347156d45de10bc9c2868c9b9ea4
AmalNadifi <amal.nadifi9@gmail.com>
2024-04-08 20:17:40 +0000

    Task2. Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,29 @@
+#!/usr/bin/yarn dev
+// Importing necessary libraries
+import { createClient, print } from 'redis';
+
+// Connecting to Redis server
+const client = createClient();
+
+// Handling connection events
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error('Redis client not connected to the server:', err.message);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.SET(schoolName, value, print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.GET(schoolName, (_err, reply) => {
+    console.log(reply);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
